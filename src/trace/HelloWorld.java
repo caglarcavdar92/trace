210d82fa1a610dccd811148d1d7bf2bda91c3e20
caglarcavdar92 <caglarcavdar92@gmail.com>
2018-03-08 16:28:21 +0300

    remove develop branch code

--- src/trace/HelloWorld.java
+++ src/trace/HelloWorld.java
@@ -4,6 +4,5 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// Prints "Hello, World" to the terminal window.
 		System.out.println("Hello, World");
-		System.out.println("Develop");
 	}
 }
